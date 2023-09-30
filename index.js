1fbf1024342dbdb91ed8a946e6ec18cea0233212
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-09-30 15:11:30 +0300

    completed assignmnent

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+//function fetchBooks() {
+  // fetch ('https://anapioficeandfire.com/api/books:')
+  //.then((res) => res.json())
+  //.then((data) => console.log(data));
   
+  //renderBooks(data);
+  
+  
+//}
+function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      renderBooks(data); // Moved it here
+    });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +29,10 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
+
+
